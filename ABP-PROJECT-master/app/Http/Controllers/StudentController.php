910314a7ff0e1f35a8c086a3e006dcb83ad42c8b
unknown <lotficrocodille@gmail.com>
2023-04-22 01:25:40 +0200

    first commit

---
+++ ABP-PROJECT-master/app/Http/Controllers/StudentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+
+class StudentController extends Controller
+{
+    public function store(Request $request) {
+        Student::create([
+          'name'=>$request->name,
+          'phone'=>$request->phone,
+          'email'=>$request->email,
+          'address'=>$request->addresse,
+        ]);
+      return redirect()->back();
+      }
+}
